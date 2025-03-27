5351034476c89ca6bd82e56e7d3acbfc2b4cf21a
Shaheel Mohamed <shaheelmohamedsaleem@gmail.com>
2025-03-27 16:30:56 +0530

    Temporary homepage

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,24 +1,36 @@
 import ProductCard from "@/components/ProductCard";
 import LongProductCard from "@/components/LongProductCard";
+import { prisma } from "@/prisma/client";
+import ImageCarousel from "@/components/ImageCarousel";
 
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    select: { id: true, name: true },
+    orderBy: {name: 'desc'},
+  });
+  const products = await prisma.product.findMany();
 
-export default function Home() {
   return (
     <div>
-        <h1>Smartphones</h1>
-        <div className="grid grid-cols-4 gap-5 mb-10">
-          <ProductCard id="b6fcfbbc-9790-4feb-b9e4-fe81908de682" />
-          <ProductCard id="bc226d93-5ef0-41db-bbf3-202ddd2c768a" />
-          <ProductCard id="733f9dd6-ca90-4363-b441-f157c9685beb" />
-          <ProductCard id="895925ae-a8f4-428d-b0c0-e1e1c34317bf" />
+      {categories.map((category) => (
+        <div key={category.name} className="mb-10">
+          <h1 className="capitalize">{category.name}</h1>
+          <div className="grid grid-cols-4 gap-5">
+            {products.map((product) =>
+              product.categoryId === category.id && (
+                <ProductCard key={product.id} id={product.id} />
+              )
+            )}
+          </div>
         </div>
-        <h1>Long Product Card</h1>
-        <div className="grid grid-cols-2 gap-5">
-          <LongProductCard id="b6fcfbbc-9790-4feb-b9e4-fe81908de682" />
-          <LongProductCard id="bc226d93-5ef0-41db-bbf3-202ddd2c768a" />
-          <LongProductCard id="733f9dd6-ca90-4363-b441-f157c9685beb" />
-          <LongProductCard id="895925ae-a8f4-428d-b0c0-e1e1c34317bf" />
-        </div>
+      ))}
+
+      <h1>Long Product Card</h1>
+      <div className="grid grid-cols-2 gap-5">
+        {products.map((product) => (
+          <LongProductCard key={product.id} id={product.id} />
+        ))}
+      </div>
     </div>
   );
 }
